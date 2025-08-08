31c0b62fabc0d79a8be5ed68617eff4fc0069855
Edson Veiga <edsonsanchezveiga@gmail.com>
2025-08-08 12:44:39 -0300

    Corrige query de pesquisa de projeto

--- system_project/src/main/java/com/project/system/repositories/ProjectRepository.java
+++ system_project/src/main/java/com/project/system/repositories/ProjectRepository.java
@@ -7,21 +7,24 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import com.project.system.entity.Occupation;
 import com.project.system.entity.Project;
 
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
 
-    @Query("SELECT p FROM Project p " +
-            "WHERE LOWER(p.projectName) LIKE LOWER(CONCAT('%', :filter, '%')) " +
-            "OR LOWER(p.projectContractualAcronym) LIKE LOWER(CONCAT('%', :filter, '%')) " +
-            "OR LOWER(p.projectBusinessVertical) LIKE LOWER(CONCAT('%', :filter, '%')) " +
-            "OR LOWER(p.projectPriority) LIKE LOWER(CONCAT('%', :filter, '%')) " +
-            "OR LOWER(p.projectStatus) LIKE LOWER(CONCAT('%', :filter, '%')) " +
-            "OR function('str', p.projectExecutionPercentage) LIKE CONCAT('%', :filter, '%') " +
-            "OR function('FORMAT', p.projectPlanningStartDate, 'yyyy-MM-dd') LIKE CONCAT('%', :filter, '%') " +
-            "OR function('FORMAT', p.projectPlanningEndDate, 'yyyy-MM-dd') LIKE CONCAT('%', :filter, '%') " +
-            "OR CAST(p.projectId AS string) LIKE CONCAT('%', :filter, '%')")
-     List<Project> searchByFilter(@Param("filter") String filter);
+	@Query("""
+			    SELECT p FROM Project p
+			    LEFT JOIN p.projectContractualAcronym a
+			    WHERE CAST(p.projectId AS string) LIKE %:filter%
+			       OR LOWER(p.projectName) LIKE LOWER(CONCAT('%', :filter, '%'))
+			       OR LOWER(a.acronym) LIKE LOWER(CONCAT('%', :filter, '%'))
+			       OR LOWER(p.projectBusinessVertical) LIKE LOWER(CONCAT('%', :filter, '%'))
+			       OR CAST(p.projectPriority AS string) LIKE %:filter%
+			       OR CAST(p.projectStatus AS string) LIKE %:filter%
+			       OR CAST(p.projectExecutionPercentage AS string) LIKE %:filter%
+			       OR CAST(p.projectPlanningStartDate AS string) LIKE %:filter%
+			       OR CAST(p.projectPlanningEndDate AS string) LIKE %:filter%
+			""")
+	List<Project> searchByFilter(@Param("filter") String filter);
+
 }
